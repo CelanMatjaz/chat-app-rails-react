0d1bf3398b84fd97bb7eab6b018cb29ea2cd93ec
CelanMatjaz <matjazcelangithub@gmail.com>
2024-05-15 20:20:10 +0200

    api: update seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -8,15 +8,36 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+require 'faker'
+
+def get_display_name
+  "#{Faker::Name.first_name} #{Faker::Name.last_name}"
+end
+
 user = User.create!(email: 'test@test.com', password: 'password', password_confirmation: 'password',
-                    display_name: 'Test User')
+                    display_name: get_display_name)
+
+channels = []
 
 %w[Room1 Room2 Room3].each do |room_name|
   room = user.rooms.create!(name: room_name, image_url: '/temp.png')
 
   user.room_user.create!(room_id: room.id)
 
-  %w[Channel1 Channel2 Channel3].each do |channel_name|
-    room.channels.create!(name: channel_name)
+  (0..3).each do |_|
+    c = room.channels.create!(name: Faker::Name.initials(number: 4))
+    channels.push(c.id)
   end
+end
+
+users = []
+
+(0..6).each do |i|
+  u = User.create!(email: "test#{i}@test.com", password: 'password', password_confirmation: 'password',
+                   display_name: get_display_name)
+  users.push(u.id)
+end
+
+(0..50).each do |_i|
+  Message.create!(text: Faker::Lorem.sentence(word_count: 20), channel_id: channels.sample, user_id: users.sample)
 end
